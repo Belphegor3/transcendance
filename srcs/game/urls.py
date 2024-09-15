57f9fbd629180f8ba2bcf986d321e111aecf852c
Belphegor3 <l.fchouch@outlook.com>
2024-09-16 01:03:29 +0200

    ajout des features du jeu mais vide encore

--- srcs/game/urls.py
+++ srcs/game/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.game, name='game'),
+    path('custom_versus/', views.custom_versus, name='custom_versus'),
+    path('custom_bot/', views.custom_bot, name='custom_bot'),
+    path('tournament/', views.tournament, name='tournament'),
+    path('launch/', views.launch, name='launch'),
 ]
--- srcs/game/views.py
+++ srcs/game/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def game(request):
     return render(request, 'game.html')
+
+def custom_versus(request):
+    return render(request, 'custom_versus.html')
+
+def custom_bot(request):
+    return render(request, 'custom_bot.html')
+
+def tournament(request):
+    return render(request, 'tournament.html')
+
+def launch(request):
+    return render(request, 'launch.html')
