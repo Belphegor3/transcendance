bb5c3334c76d22b27ab257bacdac9acf201f84ca
TheKirin80 <antoine.kefeder@gmail.com>
2024-09-19 22:45:25 +0200

    Refont architecture

--- backend/srcs/game/urls.py
+++ backend/srcs/game/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.game, name='game'),
+    path('launch/', views.launch, name='launch'),
 ]
--- backend/srcs/game/views.py
+++ backend/srcs/game/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def game(request):
     return render(request, 'game.html')
+def launch(request):
+    return render(request, 'pong.html')
