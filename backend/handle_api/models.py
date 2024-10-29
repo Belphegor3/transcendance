4b3711e0ec3ca9148f638ddf862e2f08f91d43e4
Arnaud DURAND <arnduran@paul-f4Br1s2.clusters.42paris.fr>
2024-10-29 20:03:59 +0100

    db recois info vide, il manque le signal pour lie a lutilisateur + recup les datas en json

--- backend/handle_api/models.py
+++ backend/handle_api/models.py
@@ -11,3 +11,15 @@ class GameOptions(models.Model):
 
     def __str__(self):
         return f"{self.user.username}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.jpg')
+    display_name = models.CharField(max_length=50, unique=True)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    match_history = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
--- backend/principale/urls.py
+++ backend/principale/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import handler404
 from django.urls import path, include
 from . import views
+from django.contrib import admin
+
+# user admin = admin / pass admin = 123
 
 handler404 = 'principale.views.custom_404'
 
 urlpatterns = [
     path('', views.test, name='test'),
     path('api/', include('handle_api.urls')),
+    path('admin/', admin.site.urls),
 ]
